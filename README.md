4725b66afcda59f97ceb2daa464a3d12020b3b14
Marzouq Ibrahim Mohammad Jarah <marzooqjarrah@gmail.com>
2021-07-06 15:53:03 +0300

    new game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Guessing-game
+this project shows a short brief for me and what is my top ten films i prefered.
+then shows how the user enter attempts corresponding to what i want and give him a grade according to its answers.
--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 
 let username = prompt('what is your name?   ');
 alert('welcome  ' + username + '  lets play a guessing game');
-
-let job = prompt('are you a doctor?   ');
+let score=0;
+let job = prompt('am i a doctor?   ');
 job = job.toLowerCase();
 if (job == 'yes' || job == 'y') {
     // console.log(job);
     alert('woow you are correct');
+    score++;
 }
 else if (job == 'no' || job == 'n') {
     alert('sorry, just for doctors');
@@ -18,9 +19,11 @@ earth = earth.toLowerCase();
 if (earth == 'yes' || earth == 'y') {
     // console.log(earth);
     alert('that is incorrect');
+    score++;
 }
 else if (earth == 'no' || earth == 'n') {
     alert('that is correct');
+    score++;
 }
 
 let hen = prompt('does the hen fly?   ');
@@ -28,9 +31,11 @@ hen = hen.toLowerCase();
 if (hen == 'yes' || hen == 'y') {
     // console.log(hen);
     alert('that is incorrect');
+    
 }
 else if (hen == 'no' || hen == 'n') {
     alert('that is correct');
+    score++;
 }
 
 let favclub = prompt('is my facourite club is Real madid?   ');
@@ -38,9 +43,11 @@ favclub = favclub.toLowerCase();
 if (favclub == 'yes' || favclub == 'y') {
     // console.log(favclub);
     alert('that is incorrect');
+    
 }
 else if (favclub == 'no' || favclub == 'n') {
     alert('that is correct');
+    score++;
 }
 
 let favmeal = prompt('is my facourite meal is Mansaf?   ');
@@ -48,9 +55,67 @@ favmeal = favmeal.toLowerCase();
 if (favmeal == 'yes' || favmeal == 'y') {
     // console.log(favmeal);
     alert('that is incorrect');
+    
 }
 else if (favmeal == 'no' || favmeal == 'n') {
     alert('that is correct');
+    score++;
 }
 
-    alert('thanks for playing  ' + username);
+
+let guessanum = prompt(' Guess a number between 1-10 ');
+for (let i = 0; i < 3; i++) {
+    if ( guessanum == 5) {
+        alert ('nice! you are correct');
+        score++;
+        break;
+         
+    }
+        
+    else if (guessanum > 5) {
+        alert('too high');
+         guessanum = prompt(' Guess a number between 1-10 ');
+           
+    }
+    else if (guessanum < 5) {
+        alert('too low');
+         guessanum = prompt(' Guess a number between 1-10 ');
+            
+    }
+}
+    alert('the correct answer is 5');
+
+
+
+     let food= ['shawarma', 'mansaf','burger','kabsah','zinger','pizza'];
+             
+     let favfood= prompt('what is your favourite food?  ');
+     for (let i = 0; i < 5; i++){
+         let status= true;
+         for( let j=0; j<food.length;j++){
+             if (favfood==food[j])
+             {
+                 alert('great job');
+                 status=false;
+                 score++;
+                 break;
+             }
+         }
+                        
+             if (status==true){
+                 alert('try again');
+                 favfood= prompt('what is your favourite food?  ');
+                 
+             }
+             else {
+                 break;
+             }
+                 
+             
+     }
+      console.log(food);
+
+
+
+console.log('your score is', score);
+alert('thanks for playing  ' + username);
