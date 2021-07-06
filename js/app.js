4f24ae486c09c164e6b20b62374eeedf5dde2ac0
Marzouq Ibrahim Mohammad Jarah <marzooqjarrah@gmail.com>
2021-07-06 09:47:30 +0300

    editing 2

--- js/app.js
+++ js/app.js
@@ -1,42 +1,56 @@
 'use strict'
 
-let username= prompt('what is your name?   ');
-alert ('welcome  '+username+ '  lets play a guessing game');
+let username = prompt('what is your name?   ');
+alert('welcome  ' + username + '  lets play a guessing game');
 
-let job= prompt('are you a doctor?   ');
-job=job.toLowerCase();
-if (job =='yes'|| job =='y')
-{
+let job = prompt('are you a doctor?   ');
+job = job.toLowerCase();
+if (job == 'yes' || job == 'y') {
     // console.log(job);
-alert('woow you are correct');
+    alert('woow you are correct');
 }
-else if ( job =='no'||job =='n')
-{
+else if (job == 'no' || job == 'n') {
     alert('sorry, just for doctors');
 }
 
-let earth= prompt('does the earth is flat?   ');
-earth=earth.toLowerCase();
-if (earth =='yes'|| earth =='y')
-{
+let earth = prompt('does the earth is flat?   ');
+earth = earth.toLowerCase();
+if (earth == 'yes' || earth == 'y') {
     // console.log(earth);
-alert('that is incorrect');
+    alert('that is incorrect');
 }
-else if (earth =='no'||earth =='n')
-{
+else if (earth == 'no' || earth == 'n') {
     alert('that is correct');
 }
 
-let hen= prompt('does the hen fly?   ');
-hen=hen.toLowerCase();
-if (hen =='yes'|| hen =='y')
-{
+let hen = prompt('does the hen fly?   ');
+hen = hen.toLowerCase();
+if (hen == 'yes' || hen == 'y') {
     // console.log(hen);
-alert('that is incorrect');
+    alert('that is incorrect');
 }
-else if (hen =='no'||hen =='n')
-{
+else if (hen == 'no' || hen == 'n') {
     alert('that is correct');
 }
 
-alert('thanks for playing  '+ username);
+let favclub = prompt('is my facourite club is Real madid?   ');
+favclub = favclub.toLowerCase();
+if (favclub == 'yes' || favclub == 'y') {
+    // console.log(favclub);
+    alert('that is incorrect');
+}
+else if (favclub == 'no' || favclub == 'n') {
+    alert('that is correct');
+}
+
+let favmeal = prompt('is my facourite meal is Mansaf?   ');
+favmeal = favmeal.toLowerCase();
+if (favmeal == 'yes' || favmeal == 'y') {
+    // console.log(favmeal);
+    alert('that is incorrect');
+}
+else if (favmeal == 'no' || favmeal == 'n') {
+    alert('that is correct');
+}
+
+    alert('thanks for playing  ' + username);
