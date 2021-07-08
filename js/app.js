9f98416c6d766bcd863ac3f5f08574bc98b795a5
Marzouq Ibrahim Mohammad Jarah <marzooqjarrah@gmail.com>
2021-07-08 16:19:12 +0300

    guessing with css style

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ let score=0;
 let job = prompt('am i a doctor?   ');
 job = job.toLowerCase();
 if (job == 'yes' || job == 'y') {
-    // console.log(job);
+    
     alert('woow you are correct');
     score++;
 }
@@ -17,7 +17,7 @@ else if (job == 'no' || job == 'n') {
 let earth = prompt('does the earth is flat?   ');
 earth = earth.toLowerCase();
 if (earth == 'yes' || earth == 'y') {
-    // console.log(earth);
+    
     alert('that is incorrect');
     score++;
 }
@@ -29,7 +29,7 @@ else if (earth == 'no' || earth == 'n') {
 let hen = prompt('does the hen fly?   ');
 hen = hen.toLowerCase();
 if (hen == 'yes' || hen == 'y') {
-    // console.log(hen);
+  
     alert('that is incorrect');
     
 }
@@ -41,7 +41,7 @@ else if (hen == 'no' || hen == 'n') {
 let favclub = prompt('is my facourite club is Real madid?   ');
 favclub = favclub.toLowerCase();
 if (favclub == 'yes' || favclub == 'y') {
-    // console.log(favclub);
+    
     alert('that is incorrect');
     
 }
@@ -53,7 +53,7 @@ else if (favclub == 'no' || favclub == 'n') {
 let favmeal = prompt('is my facourite meal is Mansaf?   ');
 favmeal = favmeal.toLowerCase();
 if (favmeal == 'yes' || favmeal == 'y') {
-    // console.log(favmeal);
+   
     alert('that is incorrect');
     
 }
@@ -61,6 +61,12 @@ else if (favmeal == 'no' || favmeal == 'n') {
     alert('that is correct');
     score++;
 }
+
+
+
+
+
+
 
 
 let guessanum = prompt(' Guess a number between 1-10 ');
@@ -84,6 +90,11 @@ for (let i = 0; i < 3; i++) {
     }
 }
     alert('the correct answer is 5');
+
+
+
+
+
 
 
 
@@ -113,9 +124,9 @@ for (let i = 0; i < 3; i++) {
                  
              
      }
-      console.log(food);
+     
 
 
 
-console.log('your score is', score);
+
 alert('thanks for playing  ' + username);
