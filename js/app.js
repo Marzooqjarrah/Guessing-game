5a16ad518b25bb2f5b4a4d2eeeaec20a32a764f5
Marzouq Ibrahim Mohammad Jarah <marzooqjarrah@gmail.com>
2021-07-27 09:48:38 +0300

    editing

--- js/app.js
+++ js/app.js
@@ -1,167 +1,190 @@
-'use strict'
+// 'use strict'
 
-let username = prompt('what is your name?   ');
-alert('welcome  ' + username + '  lets play a guessing game');
-let score=0;
-function quistion1(){
-let job = prompt('am i a doctor?   ');
-job = job.toLowerCase();
-if (job == 'yes' || job == 'y') {
+// let username = prompt('what is your name?   ');
+// alert('welcome  ' + username + '  lets play a guessing game');
+// let score=0;
+// function quistion1(){
+// let job = prompt('am i a doctor?   ');
+// job = job.toLowerCase();
+// if (job == 'yes' || job == 'y') {
     
-    alert('woow you are correct');
-    score++;
-}
-else if (job == 'no' || job == 'n') {
-    alert('sorry, just for doctors');
-}
-}
-quistion1();
+//     alert('woow you are correct');
+//     score++;
+// }
+// else if (job == 'no' || job == 'n') {
+//     alert('sorry, just for doctors');
+// }
+// }
+// quistion1();
 
-function quistion2(){
-let earth = prompt('does the earth is flat?   ');
-earth = earth.toLowerCase();
-if (earth == 'yes' || earth == 'y') {
+// function quistion2(){
+// let earth = prompt('does the earth is flat?   ');
+// earth = earth.toLowerCase();
+// if (earth == 'yes' || earth == 'y') {
     
-    alert('that is incorrect');
-    score++;
-}
-else if (earth == 'no' || earth == 'n') {
-    alert('that is correct');
-    score++;
-}
-}
-quistion2();
+//     alert('that is incorrect');
+//     score++;
+// }
+// else if (earth == 'no' || earth == 'n') {
+//     alert('that is correct');
+//     score++;
+// }
+// }
+// quistion2();
 
-function quistion3(){
-let hen = prompt('does the hen fly?   ');
-hen = hen.toLowerCase();
-if (hen == 'yes' || hen == 'y') {
+// function quistion3(){
+// let hen = prompt('does the hen fly?   ');
+// hen = hen.toLowerCase();
+// if (hen == 'yes' || hen == 'y') {
   
-    alert('that is incorrect');
+//     alert('that is incorrect');
     
-}
-else if (hen == 'no' || hen == 'n') {
-    alert('that is correct');
-    score++;
-}
-}
-quistion3();
+// }
+// else if (hen == 'no' || hen == 'n') {
+//     alert('that is correct');
+//     score++;
+// }
+// }
+// quistion3();
 
-function quistion4(){
-let favclub = prompt('is my facourite club is Real madid?   ');
-favclub = favclub.toLowerCase();
-if (favclub == 'yes' || favclub == 'y') {
+// function quistion4(){
+// let favclub = prompt('is my facourite club is Real madid?   ');
+// favclub = favclub.toLowerCase();
+// if (favclub == 'yes' || favclub == 'y') {
     
-    alert('that is incorrect');
+//     alert('that is incorrect');
     
-}
-else if (favclub == 'no' || favclub == 'n') {
-    alert('that is correct');
-    score++;
-}
-}
-quistion4();
+// }
+// else if (favclub == 'no' || favclub == 'n') {
+//     alert('that is correct');
+//     score++;
+// }
+// }
+// quistion4();
 
 
-function quistion5(){
-let favmeal = prompt('is my facourite meal is Mansaf?   ');
-favmeal = favmeal.toLowerCase();
-if (favmeal == 'yes' || favmeal == 'y') {
+// function quistion5(){
+// let favmeal = prompt('is my facourite meal is Mansaf?   ');
+// favmeal = favmeal.toLowerCase();
+// if (favmeal == 'yes' || favmeal == 'y') {
    
-    alert('that is incorrect');
+//     alert('that is incorrect');
     
-}
-else if (favmeal == 'no' || favmeal == 'n') {
-    alert('that is correct');
-    score++;
-}
-}
-quistion5();
+// }
+// else if (favmeal == 'no' || favmeal == 'n') {
+//     alert('that is correct');
+//     score++;
+// }
+// }
+// quistion5();
 
 
-<<<<<<< HEAD
-
-
-
+// <<<<<<< HEAD
 
 
 
-=======
-function quistion6(){
->>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
-let guessanum = prompt(' Guess a number between 1-10 ');
-for (let i = 0; i < 3; i++) {
-    if ( guessanum == 5) {
-        alert ('nice! you are correct');
-        score++;
-        break;
+
+
+
+// =======
+// function quistion6(){
+// >>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
+// let guessanum = prompt(' Guess a number between 1-10 ');
+// for (let i = 0; i < 3; i++) {
+//     if ( guessanum == 5) {
+//         alert ('nice! you are correct');
+//         score++;
+//         break;
          
-    }
+//     }
         
-    else if (guessanum > 5) {
-        alert('too high');
-         guessanum = prompt(' Guess a number between 1-10 ');
+//     else if (guessanum > 5) {
+//         alert('too high');
+//          guessanum = prompt(' Guess a number between 1-10 ');
            
-    }
-    else if (guessanum < 5) {
-        alert('too low');
-         guessanum = prompt(' Guess a number between 1-10 ');
+//     }
+//     else if (guessanum < 5) {
+//         alert('too low');
+//          guessanum = prompt(' Guess a number between 1-10 ');
             
-    }
-}
-}
-quistion6();
+//     }
+// }
+// }
+// quistion6();
 
 
-    alert('the correct answer is 5');
+//     alert('the correct answer is 5');
 
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
 
 
 
 
-=======
-function quistion7(){
->>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
-     let food= ['shawarma', 'mansaf','burger','kabsah','zinger','pizza'];
+// =======
+// function quistion7(){
+// >>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
+//      let food= ['shawarma', 'mansaf','burger','kabsah','zinger','pizza'];
              
-     let favfood= prompt('what is your favourite food?  ');
-     for (let i = 0; i < 5; i++){
-         let status= true;
-         for( let j=0; j<food.length;j++){
-             if (favfood==food[j])
-             {
-                 alert('great job');
-                 status=false;
-                 score++;
-                 break;
-             }
-         }
+//      let favfood= prompt('what is your favourite food?  ');
+//      for (let i = 0; i < 5; i++){
+//          let status= true;
+//          for( let j=0; j<food.length;j++){
+//              if (favfood==food[j])
+//              {
+//                  alert('great job');
+//                  status=false;
+//                  score++;
+//                  break;
+//              }
+//          }
                         
-             if (status==true){
-                 alert('try again');
-                 favfood= prompt('what is your favourite food?  ');
+//              if (status==true){
+//                  alert('try again');
+//                  favfood= prompt('what is your favourite food?  ');
                  
-             }
-             else {
-                 break;
-             }
+//              }
+//              else {
+//                  break;
+//              }
                  
              
-     }
-<<<<<<< HEAD
+//      }
+// <<<<<<< HEAD
      
 
 
 
 
-=======
-    }
-     quistion7();
-      console.log(food);
+// =======
+//     }
+//      quistion7();
+//       console.log(food);
 
-console.log('your score is', score);
->>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
-alert('thanks for playing  ' + username);
+// console.log('your score is', score);
+// >>>>>>> f6aaeac41e64772a5af5457cd13f0b416750a9a9
+// alert('thanks for playing  ' + username);
+
+
+
+
+// function getage (){
+//    let year = 26;
+//    let msg = 'your name';
+//    let x='wow'
+//     return [year,msg,x];
+// }
+// console.log(getage());
+// console.log(getage()[0]);
+// console.log( getage()[2]);
+// console.log(getage ()[1]);
+
+
+// function sum(a,b){
+// let res= a+b;
+// return[res ,`the sum of ${a} and ${b} is ${res}`]
+// }
+// console.log(sum(7,9));
+
+function mul(a,b)
